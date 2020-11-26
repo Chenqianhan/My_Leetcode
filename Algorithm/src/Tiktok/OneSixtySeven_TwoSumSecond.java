1b5526e0546d81e27954efd9545b51d329f03326
真男人从不用IDE <chenqianhan@outlook.com>
2020-11-26 00:49:28 -0500

    Update

---
+++ Algorithm/src/Tiktok/OneSixtySeven_TwoSumSecond.java
@@ -0,0 +1,17 @@
+package Tiktok;
+
+public class OneSixtySeven_TwoSumSecond {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length-1;
+        while(left < right){
+            if(numbers[left]+numbers[right]<target)
+                left++;
+            else if(numbers[left]+numbers[right]>target)
+                right--;
+            else
+                return new int[]{left+1, right+1};
+        }
+
+        return null;
+    }
+}
